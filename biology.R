b1c10c60bd7cde122c192f761b3867ecae9dd347
Ilya Boyko <jehll@users.noreply.github.com>
2017-02-15 03:46:47 +0700

    Update biology.R

--- biology.R
+++ biology.R
@@ -3,6 +3,8 @@ library(dplyr)
 library(psych)
 library(MVN)
 library(ggplot2)
+library(factoextra)
+library(ggbiplot)
 
 setwd("C:/biology")
 
@@ -28,3 +30,16 @@ uniNorm(num_data, type = "Lillie", desc = FALSE) # MVN
 
 source("make_histogram.R")
 make_histogram(dataset, 3)
+
+pc <- prcomp(num_data, center = T, scale. = T)
+summary(pc)
+
+pc$rotation
+pc$x
+
+ggbiplot(pc, choices = c(1,2), obs.scale = 1, var.scale = 1,
+         groups = species, ellipse = T)
+
+fviz_contrib(pc, choice = "var", axes = 1:4)
+
+
