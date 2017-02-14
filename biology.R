c47f7b37ca2de10b59bbc7eac236a483f34b8d0e
Ilya Boyko <jehll@users.noreply.github.com>
2017-02-14 23:54:47 +0700

    Update biology.R

--- biology.R
+++ biology.R
@@ -18,6 +18,12 @@ descr_stat <- round(describe(num_data), 2) # psych
 source("correlations.R")
 cor_list <- correlations(num_data)
 
+qplot(data = num_data,
+      length.of.the.anthecium,
+      length.of.the.callus) + stat_smooth(method = "lm")
+
+frequency_table <- count_(dataset, "species", sort = T)
+
 uniNorm(num_data, type = "Lillie", desc = FALSE) # MVN
 
 source("make_histogram.R")
