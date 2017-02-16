570a47cc524c45ebffff75f03090a33636e7c444
Ilya Boyko <jehll@users.noreply.github.com>
2017-02-17 01:40:46 +0700

    Update correlations.R

--- correlations.R
+++ correlations.R
@@ -1,9 +1,14 @@
 correlations <- function(data)
 {
-  correlation_matrix <- round(cor(data, method = "pearson"),2)
+  correlation_matrix <- round(cor(data, method = "pearson"), 3)
   diag(correlation_matrix) <- 0
   correlation_matrix[lower.tri(correlation_matrix)] <- 0
   fm <- as.data.frame(as.table(correlation_matrix))
   names(fm) <- c("First.Variable", "Second.Variable","Correlation")
-  return(fm[order(abs(fm$Correlation),decreasing=T),])
+  sorted <- fm[order(abs(fm$Correlation),decreasing=T),]
+  i = 1
+  while(sorted[i,3] != 0){
+    i = i+1
+  }
+  return(sorted[1:i-1,])
 }
