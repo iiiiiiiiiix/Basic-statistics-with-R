b9d01d9d8340e5815eb3cc024200465eef634531
Ilya Boyko <jehll@users.noreply.github.com>
2017-02-20 01:59:28 +0700

    Update make_histogram.R

--- make_histogram.R
+++ make_histogram.R
@@ -2,7 +2,8 @@ make_histogram <- function(data, variable)
 {
   d <- data[,c(1,variable)]
   values <- as.vector(as.matrix(d[,2]))
-  hist <- ggplot(d, aes(values, fill = data$species, colour = data$species)) +
+  species <- data$species
+  hist <- ggplot(d, aes(values, fill = species, colour = species)) +
     geom_histogram(alpha = 0.7, bins = 14) + ggtitle(colnames(d)[2])
   print(hist)
 }
