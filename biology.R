5a41c51d0be3ffea8517bdd635af678378470f2f
Ilya Boyko <jehll@users.noreply.github.com>
2017-02-20 17:13:26 +0700

    Update biology.R

--- biology.R
+++ biology.R
@@ -5,6 +5,9 @@ library(MVN)
 library(ggplot2)
 library(factoextra)
 library(ggbiplot)
+library(cluster)
+library(ggdendro)
+
 
 setwd("C:/biology")
 
@@ -52,8 +55,15 @@ fviz_contrib(pc, choice = "var", axes = 1:4)
 
 frequency_table <- count_(dataset, "species", sort = T)
 
+cluster_data <- select(dataset, -species, -class)
 
+dm <- daisy(cluster_data, metric = "gower")
 
+tree <- hclust(dm, method = "average") # "average" = метод UPGMA
+plot(as.dendrogram(tree))
+rect.hclust(tree, 5, border="blue")
 
+result <- cutree(tree, 5)
+table(dataset$species, result)
 
 detach(dataset)
